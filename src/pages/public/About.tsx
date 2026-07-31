dcff63b3300d9d3117ddc11262c944dd7fc72891
Rhea Rodrigues <rhearodrigues106@gmail.com>
2026-07-31 20:46:27 +0530

    Update website favicon again

--- src/pages/public/About.tsx
+++ src/pages/public/About.tsx
@@ -148,8 +148,8 @@ const About = () => {
             ) : (
               <>
                 <StatCard value={2000} label="Businesses Served" icon={<Users size={22} />} suffix="+" />
-                <StatCard value={14} label="Years Experience" icon={<Clock size={22} />} suffix="+" />
-                <StatCard value={50} label="Products" icon={<Award size={22} />} suffix="+" />
+<StatCard value={14} label="Years Experience" icon={<Clock size={22} />} suffix="+" />
+<StatCard value={8} label="Products" icon={<Award size={22} />} suffix="+" />
                 <div className="text-center p-4 rounded-2xl bg-gray-50 border border-gray-100/80 shadow-xs">
                   <div className="w-12 h-12 bg-blue-100 text-blue-700 rounded-xl flex items-center justify-center mx-auto mb-3 shadow-xs">
                     <Shield size={22} />
